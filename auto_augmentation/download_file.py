6ea8858ee3fc923781cc9672f2fbe28e9879028a
Mia Wang <yw21218@ic.ac.uk>
2022-01-27 11:03:47 +0000

    download button

---
+++ auto_augmentation/download_file.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, request, render_template, flash, send_file
+
+bp = Blueprint("download_file", __name__)
+
+@bp.route("/download_file", methods=["GET"])
+@bp.route("/choose_file", methods=["GET", "POST"])
+def download():    
+    # Setup for the 'return send_file()' function call at the end of this function
+    path = 'templates/CNN.zip' # e.g. 'templates/download.markdown'
+    
+    return send_file(path,
+                    as_attachment=True)
