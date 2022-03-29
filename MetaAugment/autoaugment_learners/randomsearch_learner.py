37f9713fafa4ad128c84b765d37e0ffe41c43043
Sun Jin Kim <sk2521@ic.ac.uk>
2022-03-29 16:08:14 +0100

    random search learner first version complete

--- MetaAugment/autoaugment_learners/randomsearch_learner.py
+++ MetaAugment/autoaugment_learners/randomsearch_learner.py
@@ -1,3 +1,4 @@
+from importlib import machinery
 import torch
 import numpy as np
 from MetaAugment.main import *
@@ -8,7 +9,11 @@ from MetaAugment.autoaugment_learners.autoaugment import *
 import torchvision.transforms.autoaugment as torchaa
 from torchvision.transforms import functional as F, InterpolationMode
 
-policies1 = [
+from pprint import pprint
+
+
+# an example of how a policy can look like
+policy1 = [
             (("Invert", 0.8, None), ("Contrast", 0.2, 6)),
             (("Rotate", 0.7, 2), ("Invert", 0.8, None)),
             (("Sharpness", 0.8, 1), ("Sharpness", 0.9, 3)),
@@ -17,10 +22,10 @@ policies1 = [
             ]
 
 # We will use this augmentation_space temporarily. Later on we will need to 
-# make sure we are able to add other image operations if the users want.
+# make sure we are able to add other image functions if the users want.
 num_bins = 10
 augmentation_space = [
-            # (operation_name, do_we_need_to_specify_magnitude)
+            # (function_name, do_we_need_to_specify_magnitude)
             ("ShearX", True),
             ("ShearY", True),
             ("TranslateX", True),
@@ -45,9 +50,12 @@ class randomsearch_learner:
         '''
         self.sp_num = sp_num
 
-        # op_num is the number of different operations
-        # TODO: Allow op_num to be changed with the user's specifications 
-        self.op_num = 14
+        # fun_num is the number of different operations
+        # TODO: Allow fun_num to be changed with the user's specifications 
+        self.fun_num = 14
+
+        # TODO: We should probably use a different way to store results than self.history
+        self.history = []
 
     def generate_new_policy(self):
         '''
@@ -60,104 +68,108 @@ class randomsearch_learner:
             ]
         '''
 
-
-        def generate_new_discrete_subpolicy(op_num=14, p_bins=10, m_bins=10):
+        def generate_new_discrete_operation(fun_num=14, p_bins=10, m_bins=10):
             '''
-            generate a new random subpolicy in the form of a tensor of dimension:
-                2* (op_num + 11 + 10)
-            Where the 2 represents the 2 operations. 
+            generate a new random operation in the form of a tensor of dimension:
+                (fun_num + 11 + 10)
 
-            And for each operation, the first op_num dimensions are hard-maxed to specify which operation to use.
+            The first fun_num dimensions is a 1-hot encoding to specify which function to use.
             The next 11 dimensions specify which 'probability' to choose.
                 (0.0, 0.1, ..., 1.0)
             The next 10 dimensions specify which 'magnitude' to choose.
                 (0, 1, ..., 9)
             '''
-            op = np.random.randint(0, op_num)
-            prob = np.random.randint(p_bins+1, op_num)
-            mag = np.random.randint(m_bins, op_num)
+            fun = np.random.randint(0, fun_num)
+            prob = np.random.randint(p_bins+1, fun_num)
+            mag = np.random.randint(m_bins, fun_num)
             
-            op_t= torch.zeros(op_num)
-            op_t[op] = 1
+            fun_t= torch.zeros(fun_num)
+            fun_t[fun] = 1
             prob_t = torch.zeros(p_bins+1)
+            prob_t[prob] = 1
             mag_t = torch.zeros(m_bins)
+            mag_t[mag] = 1
 
-            return torch.cat([op_t, prob_t, mag_t])
+            return torch.cat([fun_t, prob_t, mag_t])
         
 
-        def generate_new_continuous_subpolicy(op_num=14, p_bins=10, m_bins=10):
+        def generate_new_continuous_operation(fun_num=14, p_bins=10, m_bins=10):
             '''
-            Returns subpolicy_tensor, which is a tensor representation of a random subpolicy with
+            Returns operation_tensor, which is a tensor representation of a random operation with
             dimension:
-                2 * (op_num + 1 + 1)
-            Where the 2 represents the 2 operations. 
+                (fun_num + 1 + 1)
 
-            And for each operation, the first op_num dimensions are hard-maxed to specify which operation to use.
+            The first fun_num dimensions is a 1-hot encoding to specify which function to use.
             The next 1 dimensions specify which 'probability' to choose.
                 0 < x < 1
             The next 1 dimensions specify which 'magnitude' to choose.
                 0 < x < 9
-            
-            Yes, the inequalities are strict, but we will round them up so that they go 
-            to the nearest integers, so it will be possible for the magnitude to be 0 or 9
             '''
-            def get_op_p_m():
-                op = np.random.randint(0, op_num)
-                prob = np.random.randint(p_bins+1, op_num)
-                mag = np.random.randint(m_bins, op_num)
-                
-                op_p_m = torch.zeros(op_num + 2)
-                op_p_m[op] = 1
-                op_p_m[-2] = np.random.uniform()
-                op_p_m[-1] = np.random.uniform() * 9
-
-                return op_p_m
+            fun = np.random.randint(0, fun_num)
             
-            return torch.cat([get_op_p_m, get_op_p_m])
-        
-        def translate_subpolicy_tensor(subpolicy_tensor, op_num=14,
+            fun_p_m = torch.zeros(fun_num + 2)
+            fun_p_m[fun] = 1
+            fun_p_m[-2] = np.random.uniform() # 0<prob<1
+            fun_p_m[-1] = np.random.uniform() * (m_bins-1) # 0<mag<9
+            
+            return fun_p_m
+
+
+        def translate_operation_tensor(operation_tensor, fun_num=14,
                                         p_bins=10, m_bins=10,
                                         discrete_p_m=False):
             '''
-            takes in a tensor representing a subpolicy and returns an actual subpolicy which
+            takes in a tensor representing a operation and returns an actual operation which
             is in the form of:
-                (("Invert", 0.8, None), ("Contrast", 0.2, 6)))
+                ("Invert", 0.8, None)
+                or
+                ("Contrast", 0.2, 6)
 
             Args:
-                subpolicy_tensor
-                continuous_p_m (boolean): whether the subpolicy_tensor has continuous representations
+                operation_tensor
+                continuous_p_m (boolean): whether the operation_tensor has continuous representations
                                         of probability and magnitude
             '''
-            # if input subpolicy_tensor is continuous
-            if not discrete_p_m:
-                op_t = subpolicy_tensor[:op_num]
-                prob_t = subpolicy_tensor[op_num:op_num+p_bins+1]
-                mag_t = subpolicy_tensor[-m_bins:]
-                raise NotImplementedError, "U can implement this if u want"
-            
-            # process continuous subpolicy_tensor
-            op_t = subpolicy_tensor[:op_num]
-            p = subpolicy_tensor[-2] # 0 < p < 1
-            m = subpolicy_tensor[-1] # 0 < m < 9
+            # if input operation_tensor is continuous
+            if discrete_p_m:
+                fun_t = operation_tensor[:fun_num]
+                prob_t = operation_tensor[fun_num:fun_num+p_bins+1]
+                mag_t = operation_tensor[-m_bins:]
 
-            op_num = torch.argmax(op_t)
+                fun = torch.argmax(fun_t)
+                prob = torch.argmax(prob_t)
+                mag = torch.argmax(mag_t)
+                raise NotImplementedError("U can implement this if u want")
+            
+            # process continuous operation_tensor
+            fun_t = operation_tensor[:fun_num]
+            p = operation_tensor[-2].item() # 0 < p < 1
+            m = operation_tensor[-1].item() # 0 < m < 9
+
+            fun_num = torch.argmax(fun_t)
+            function = augmentation_space[fun_num][0]
             p = round(p, 1) # round to nearest first decimal digit
             m = round(m) # round to nearest integer
-
-
-            # take argmax
+            return (function, p, m)
 
 
         new_policy = []
         for _ in range(self.sp_num):
-            new_subpolicy = generate_new_continuous_subpolicy(self.op_num)
+            # generate 2 operations for each subpolicy
+            ops = []
+            for i in range(2):
+                new_op = generate_new_continuous_operation(self.fun_num)
+                new_op = translate_operation_tensor(new_op)
+                ops.append(new_op)
 
+            new_subpolicy = tuple(ops)
 
             new_policy.append(new_subpolicy)
 
         return new_policy
 
-    def learn(self, train_dataset, test_dataset, child_network, toy_flag):
+
+    def learn(self, train_dataset, test_dataset, child_network_architecture, toy_flag):
         '''
         Does the loop which is seen in Figure 1 in the AutoAugment paper.
         In other words, repeat:
@@ -170,15 +182,15 @@ class randomsearch_learner:
         for _ in range(15):
             policy = self.generate_new_policy()
 
+            pprint(policy)
+            child_network = child_network_architecture()
             reward = self.test_autoaugment_policy(policy, child_network, train_dataset,
                                                 test_dataset, toy_flag)
 
-            self.controller.update(reward, policy)
-        
-
+            self.history.append((policy, reward))
     
 
-    def test_autoaugment_policy(self, policy, child_network, train_dataset, test_dataset):
+    def test_autoaugment_policy(self, policy, child_network, train_dataset, test_dataset, toy_flag):
         '''
         Given a policy (using AutoAugment paper terminology), we train a child network
         with the policy and return the accuracy.
@@ -210,7 +222,7 @@ class randomsearch_learner:
                                     sgd = optim.SGD(child_network.parameters(), lr=1e-1),
                                     cost = nn.CrossEntropyLoss(),
                                     max_epochs = 100, 
-                                    early_stop_num = 10, 
+                                    early_stop_num = 15, 
                                     logging = False)
         return accuracy
 
@@ -224,9 +236,9 @@ if __name__=='__main__':
                                 transform=None)
     test_dataset = datasets.MNIST(root='./datasets/mnist/test', train=False, download=False,
                                 transform=torchvision.transforms.ToTensor())
-    child_network = cn.lenet()
+    child_network = cn.lenet
 
     
     rs_learner = randomsearch_learner()
     rs_learner.learn(train_dataset, test_dataset, child_network, toy_flag=True)
-    print(rs_learner.best_five)
+    pprint(rs_learner.history)
