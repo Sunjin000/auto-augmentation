70d8d4de2aa83b5ad602b76c059e47e94a19d5ff
Sun Jin Kim <sk2521@ic.ac.uk>
2022-04-07 10:12:31 +0900

    delete flask app stuff from repo. I forked them all to another repo

--- auto_augmentation/result.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request, render_template, flash, send_file
-import subprocess
-
-bp = Blueprint("result", __name__)
-
-@bp.route("/show_result", methods=["GET", "POST"])
-def response():
-    
-    return render_template("result.html")
