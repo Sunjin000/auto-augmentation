c0d18959f4188228b6d3df12ff3d4b2fdb97d294
Mia Wang <yw21218@ic.ac.uk>
2022-04-13 21:50:56 +0100

    add web app stuff

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from auto_augmentation import create_app
+import os
+app = create_app()
+port = int(os.environ.get("PORT", 5000))
+
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0',port=port)
+
+if __name__ == '__main__':
+    app.run(debug=True)
