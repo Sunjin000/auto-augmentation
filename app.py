1fcdbddd311c83cf8c25341afe60497051dcb113
Max Ramsay King <maxramsayking@gmail.com>
2022-04-14 16:41:55 -0700

    got first page of web app working

--- app.py
+++ app.py
@@ -17,32 +17,7 @@ app = create_app()
 port = int(os.environ.get("PORT", 5000))
 
 
-UPLOAD_FOLDER = '/datasets'
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-ALLOWED_EXTENSIONS = {'pdf', 'py'}
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/user_input', methods = ['GET', 'POST'])
-def upload_file():
-    print("HELLoasdjsadojsadojsaodjsaoij")
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
-    return '''
-    
-    '''
 
 
 if __name__ == '__main__':
--- auto_augmentation/progress.py
+++ auto_augmentation/progress.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, render_template, flash, send_file
 import subprocess
 import os
+import zipfile
 
 import numpy as np
 import torch
@@ -17,39 +18,92 @@ from tqdm import trange
 torch.manual_seed(0)
 # import agents and its functions
 
-from MetaAugment import UCB1_JC as UCB1_JC
+from MetaAugment import UCB1_JC_py as UCB1_JC
 
 
 
 bp = Blueprint("progress", __name__)
 
+
 @bp.route("/user_input", methods=["GET", "POST"])
 def response():
 
     # hyperparameters to change
-    batch_size = 32       # size of batch the inner NN is trained with
-    learning_rate = 1e-1  # fix learning rate
-    ds = request.args["dataset_selection"]        # pick dataset (MNIST, KMNIST, FashionMNIST, CIFAR10, CIFAR100)
-    toy_size = 0.02       # total propeortion of training and test set we use
-    max_epochs = 100      # max number of epochs that is run if early stopping is not hit
-    early_stop_num = 10   # max number of worse validation scores before early stopping is triggered
-    num_policies = 5      # fix number of policies
-    num_sub_policies = 5  # fix number of sub-policies in a policy
-    iterations = 100      # total iterations, should be more than the number of policies
-    IsLeNet = request.args["network_selection"]   # using LeNet or EasyNet or SimpleNet ->> default 
+    # print("thing: ", request.files['dataset_upload'] )
+    if request.method == 'POST':
+        batch_size = 1       # size of batch the inner NN is trained with
+        learning_rate = 1e-1  # fix learning rate
+        ds = request.form.get("dataset_selection")      # pick dataset (MNIST, KMNIST, FashionMNIST, CIFAR10, CIFAR100)
+        toy_size = 1      # total propeortion of training and test set we use
+        max_epochs = 10      # max number of epochs that is run if early stopping is not hit
+        early_stop_num = 10   # max number of worse validation scores before early stopping is triggered
+        num_policies = 5      # fix number of policies
+        num_sub_policies = 5  # fix number of sub-policies in a policy
+        iterations = 10      # total iterations, should be more than the number of policies
+        IsLeNet = request.form.get("network_selection")   # using LeNet or EasyNet or SimpleNet ->> default 
 
-    print(f'@@@@@ dataset is: {ds}, network is :{IsLeNet}')
+        print(f'@@@@@ dataset is: {ds}, network is :{IsLeNet}')
 
-    # generate random policies at start
-    policies = UCB1_JC.generate_policies(num_policies, num_sub_policies)
+        # if user upload datasets and networks, save them in the database
+        if ds == 'Other':
+            ds_folder = request.files['dataset_upload']
+            print('@@@ ds_folder', ds_folder)
+            ds_name_zip = ds_folder.filename
+            ds_folder.save('./MetaAugment/datasets/'+ ds_name_zip)
+            with zipfile.ZipFile('./MetaAugment/datasets/'+ ds_name_zip, 'r') as zip_ref:
+                zip_ref.extractall('./MetaAugment/datasets/')
+            ds_name = ds_name_zip.split('.')[0]
 
-    q_values, best_q_values = UCB1_JC.run_UCB1(policies, batch_size, learning_rate, ds, toy_size, max_epochs, early_stop_num, iterations, IsLeNet)
+        else: 
+            ds_name = None
 
-    plt.plot(best_q_values)
+        
+        if IsLeNet == 'Other':
+            childnetwork = request.files['network_upload']
+            childnetwork.save('./MetaAugment/child_networks/'+childnetwork.filename)
 
-    best_q_values = np.array(best_q_values)
-    save('best_q_values_{}_{}percent_{}.npy'.format(IsLeNet, int(toy_size*100), ds), best_q_values)
-    #best_q_values = load('best_q_values_{}_{}percent_{}.npy'.format(IsLeNet, int(toy_size*100), ds), allow_pickle=True)
+        
+        # generate random policies at start
+        policies = UCB1_JC.generate_policies(num_policies, num_sub_policies)
+        q_values, best_q_values = UCB1_JC.run_UCB1(policies, batch_size, learning_rate, ds, toy_size, max_epochs, early_stop_num, iterations, IsLeNet, ds_name)
+
+        # plt.plot(best_q_values)
+
+        best_q_values = np.array(best_q_values)
+        # save('best_q_values_{}_{}percent_{}.npy'.format(IsLeNet, int(toy_size*100), ds), best_q_values)
+        #best_q_values = load('best_q_values_{}_{}percent_{}.npy'.format(IsLeNet, int(toy_size*100), ds), allow_pickle=True)
+        print("DONE")
 
 
     return render_template("progress.html")
+
+
+
+
+
+########### TESTING STUFF
+
+# UPLOAD_FOLDER = '/datasets'
+
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# def allowed_file(filename):
+#     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# @app.route('/user_input', methods = ['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         if 'file' not in request.files:
+#             flash('No file part')
+#             return redirect(request.url)
+#         file = request.files['file']
+#         if file.filename == '':
+#             flash('No selected file')
+#             return redirect(request.url)
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#             return redirect(url_for('uploaded_file', filename=filename))
+#     return '''
+    
+#     '''
