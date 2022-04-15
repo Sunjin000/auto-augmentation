982fc3d7418a62512ce7c7eb2cd653277a8fbed7
Sun Jin Kim <sk2521@ic.ac.uk>
2022-04-15 16:51:24 +0100

    add logo to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -74,8 +74,8 @@ html_theme = 'sphinx_rtd_theme'
 # html_theme_path = ["_themes", ]
 html_theme_options = {
     'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    'logo_only': True,
     'analytics_anonymize_ip': False,
-    'logo_only': False,
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
@@ -86,8 +86,11 @@ html_theme_options = {
     'sticky_navigation': True,
     'navigation_depth': 4,
     'includehidden': True,
-    'titles_only': False
+    'titles_only': False,
 }
+
+# logo
+html_logo = "_static/logohd.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
