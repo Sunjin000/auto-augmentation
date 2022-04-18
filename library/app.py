22a831e756df48e542d601d1920dcd90d3fa3295
Mia Wang <yw21218@ic.ac.uk>
2022-04-18 13:01:14 +0100

    change backbone

--- library/app.py
+++ library/app.py
@@ -17,7 +17,5 @@ app = create_app()
 port = int(os.environ.get("PORT", 5000))
 
 
-
-
 if __name__ == '__main__':
     app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -7,32 +7,59 @@ import axios from "axios";
 
 function App() {
 
-  // const [formData, setFormData] = useState(null)
+  const [formData, setFormData] = useState(null)
 
-  // function getData() {
-  //   axios({
-  //     method: "POST",
-  //     url:"/profile",
-  //   })
-  //   .then((response) => {
-  //     const res =response.data
-  //     setProfileData(({
-  //       profile_name: res.name,
-  //       about_me: res.about}))
-  //   }).catch((error) => {
-  //     if (error.response) {
-  //       console.log(error.response)
-  //       console.log(error.response.status)
-  //       console.log(error.response.headers)
-  //       }
-  //   })}
-    //end of new line 
+  function getData() {
+    axios({
+      method: "POST",
+      url:"/profile",
+    })
+    .then((response) => {
+      const res =response.data
+      setProfileData(({
+        profile_name: res.name,
+        about_me: res.about}))
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+        }
+    })}
 
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+  
+          {/* new line start*/}
+          <p>To get your profile details: </p><button onClick={getData}>Click me</button>
+          {profileData && <div>
+                <p>Profile name: {profileData.profile_name}</p>
+                <p>About me: {profileData.about_me}</p>
+              </div>
+          }
+           {/* end of new line */}
+        </header>
+      </div>
+    );
+
+  // return (
+  //   <div>
+  //     <Home />
+  //   </div>
+  // );
 }
 
 export default App;
