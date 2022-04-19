5b0a38dc791a322a30f93b666858a0fcf4e27840
Mia Wang <yw21218@ic.ac.uk>
2022-04-19 14:35:04 +0100

    react flask

--- backend/react_app.py
+++ backend/react_app.py
@@ -1,8 +1,7 @@
 from flask import Flask, request
-from flask_cors import CORS
+# from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 
 @app.route('/profile')
--- src/App.js
+++ src/App.js
@@ -7,72 +7,22 @@ import logo from './logo.svg';
 // import {BrowerRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
-  // new line start
- const [profileData, setProfileData] = useState(null)
+  useEffect(() => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ })
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
 
- function getData() {
-   axios({
-     method: "GET",
-     url:"/profile",
-   })
-   .then((response) => {
-     const res =response.data
-     setProfileData(({
-       profile_name: res.name,
-       about_me: res.about}))
-   }).catch((error) => {
-     if (error.response) {
-       console.log(error.response)
-       console.log(error.response.status)
-       console.log(error.response.headers)
-       }
-   })}
-   //end of new line 
-
- return (
-   <div className="App">
-     <header className="App-header">
-       <img src={logo} className="App-logo" alt="logo" />
-       <p>
-         Edit <code>src/App.js</code> and save to reload.
-       </p>
-       <a
-         className="App-link"
-         href="https://reactjs.org"
-         target="_blank"
-         rel="noopener noreferrer"
-       >
-         Learn React
-       </a>
-
-       {/* new line start*/}
-       <p>To get your profile details: </p><button onClick={getData}>Click me</button>
-       {profileData && <div>
-             <p>Profile name: {profileData.profile_name}</p>
-             <p>About me: {profileData.about_me}</p>
-           </div>
-       }
-        {/* end of new line */}
-     </header>
-   </div>
- );
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
-//   useEffect(() => {
-//     fetch('/', {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify({ })
-//     })
-//       .then((response) => response.json())
-//       .then((data) => console.log(data));
-//   }, []);
-
-//   return (
-//     <div>
-//       <Home />
-//     </div>
-//   );
-// }
 
 
 export default App;
